32563be6748cc1124efa6ad8f206b27ce7c8d9e1
Mikel Goyeneche <info@mikelweb.net>
2025-11-17 08:54:55 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// Node server
+const app = express();
+// Server port
+const port = 3900;
+
+// Convert body to JS Obj
+app.use(express.json());
+// Get x-www-urlencoded
+app.use(express.urlencoded({extended: true}));
+app.listen(port, () => {
+    console.log("Server running on port " + port)
+});
