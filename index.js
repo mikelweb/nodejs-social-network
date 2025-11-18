da40599eb12f7d8e1165dd570f99b43548fd45eb
Mikel Goyeneche <info@mikelweb.net>
2025-11-18 08:56:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { conn } = require("./database/conn");
 const express = require("express");
+const cors = require("cors");
+
+// DB connection
 conn();
 
 // Node server
 const app = express();
 // Server port
 const port = 3900;
+
+// Configure CORS
+app.use(cors());
 
 // Convert body to JS Obj
 app.use(express.json());
