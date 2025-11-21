c9ca2dc35a446648fd058959790cf36e82ea21a0
Mikel Goyeneche <info@mikelweb.net>
2025-11-21 08:58:54 +0100

    Create user.js

---
+++ model/user.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String
+    },
+    nick: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "role_user"
+    },
+    image: {
+        type: String,
+        default: "default.png"
+    },
+    registered: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = model("User", UserSchema, "users")
