4f9da0370ccb1cd896d8eab8979b757a0377ac2f
Mikel Goyeneche <info@mikelweb.net>
2025-11-28 12:36:59 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,5 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/User")
 
-router.get("/prueba-user", UserController.pruebaUser);
 
 module.exports = router;
