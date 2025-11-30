8042f66744f1eb63991d83045f9db08fac46df75
Mikel Goyeneche <info@mikelweb.net>
2025-11-30 12:38:45 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/User")
 
+router.post("/register", UserController.register);
 
 module.exports = router;
