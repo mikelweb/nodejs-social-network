b3fc7ddfa25b29f5e7191216152a67396c692d89
Mikel Goyeneche <info@mikelweb.net>
2025-11-30 12:38:39 +0100

    Update User.js

--- controller/User.js
+++ controller/User.js
@@ -1,6 +1,16 @@
 const User = require("../model/user")
 const bcrypt = require("bcrypt")
 
+const register = async (req, res) => {
+    // get req data
+    const params = req.body;
+
+    if(!params.name || !params.surname || !params.nick || !params.password) {
+        return res.status(400).json({
+            "status": "error",
+            "message": "Faltan datos de registro de usuario"
+        });
+    }
         }
     );
 }
