4f425e29c608f8a7c6c1d9a3f8379ccc03892c2a
Mikel Goyeneche <info@mikelweb.net>
2025-12-09 07:59:54 +0100

    add profile route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/User")
+const auth = require("../middleware/auth")
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.get("/profile/:nick", auth.auth, UserController.profile);
 
 module.exports = router;
