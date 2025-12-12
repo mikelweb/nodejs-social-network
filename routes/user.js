35feb03284fe6f42f952c2bfa83cd97ff1522623
Mikel Goyeneche <info@mikelweb.net>
2025-12-12 08:03:02 +0100

    add list method routes

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,8 @@ const auth = require("../middleware/auth")
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/profile/:nick", auth.auth, UserController.profile);
+// Two possible cases, with or without param
+router.get("/list", auth.auth, UserController.list);
+router.get("/list/:page", auth.auth, UserController.list);
 
 module.exports = router;
